83184abdb011a3cfc4412298394d30a10053bf29
istvan <istvan.madari@vanderbilt.edu>
2018-09-17 13:03:59 -0500

    add hpp/pc to the output

--- conanfile.py
+++ conanfile.py
@@ -42,12 +42,10 @@ class MsgpackConan(ConanFile):
 
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.h", dst="include", src="msgpack-c/include")
+        self.copy("*.hpp", dst="include", src="msgpack-c/include")
         self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.pc", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["msgpackc"]
